b75a6dc72917022d6d4f6d9ffcc3bba60a5dd20e
Abdelrhamn Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-03-16 20:38:38 +0200

    NVIC Driver using struct

---
+++ NVIC_struct/NVIC_config.h
@@ -0,0 +1,22 @@
+/* **************************************************************************************
+ * NVIC_config.h																		*
+ *																						*
+ *  Created on: Mar 16, 2021															*
+ *      Author: Elgharib																*
+ * **************************************************************************************/
+
+#ifndef NVIC_CONFIG_H_
+#define NVIC_CONFIG_H_
+
+/**
+ * Number f Groups and sub groups can be:
+ * 1- _16_GROUPS_0_SUB
+ * 2- _8_GROUPS_2_SUB 
+ * 3- _4_GROUPS_4_SUB 
+ * 4- _2_GROUPS_8_SUB 
+ * 5- _0_GROUPS_16_SUB
+*/
+#define     NVIC_NO_GROUPS_SUB          _4_GROUPS_4_SUB
+
+
+#endif /* NVIC_CONFIG_H_ */
